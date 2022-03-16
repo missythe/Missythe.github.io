8dfc05ae6b985e3918ba1cd4962b22fe2ee71771
Jianfan <101674246+missythe@users.noreply.github.com>
2022-03-16 08:45:53 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function playMusic() {
+        var audioEle = document.getElementById("audio");
+      if (audioEle.paused){
+        audioEle.play();
+      }else {
+        audioEle.pause();
+      }
+    }
+    
+    $("#music").click(function(){
+      play();
+    });
